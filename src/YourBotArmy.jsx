e830d953110ddcaeab250d05f6a125ab0baadef0
nicksonmogaka <nickson.mogaka@student.moringaschool.com>
2024-08-18 12:54:36 +0300

    Update YourBotArmy.jsx

--- src/YourBotArmy.jsx
+++ src/YourBotArmy.jsx
@@ -1,18 +1,15 @@
 import React from 'react';
 import SingleBot from './SingleBot';
 
-function YourBotArmy({ bots  }) {
-  return (
-    <div className='card'>
-      <h2>BOT ARMY</h2>
-      <div>
-        
-        {bots.map(bot => (
-          <SingleBot key={bot.id} bot={bot}  />
-        ))}
-      </div>
+const YourBotArmy = ({ bots }) => (
+  <div className='card'>
+    <h2>BOT ARMY</h2>
+    <div>
+      {bots.map(bot => (
+        <SingleBot key={bot.id} bot={bot} />
+      ))}
     </div>
-  );
-}
+  </div>
+);
 
 export default YourBotArmy;
